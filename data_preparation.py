9ff27cc20ad5dcee2ece623b7632583a8fc3de85
unknown <daszmart1@gmail.com>
2024-05-22 00:54:06 +0200

    fuzzy training direct testing approach added

--- data_preparation.py
+++ data_preparation.py
@@ -33,6 +33,21 @@ def normalize_data(data: np.ndarray):
     
     return norm_data
 
+def add_fuzzy_scores(data: pd.core.frame.DataFrame, fuzzy_path: str):
+    # funcion should be applied after specifying the output:
+    with open(fuzzy_path, 'r') as file:
+        fuzzy_scores = file.readlines()
+    fuzzy_scores = np.array([float(line.strip()) for line in fuzzy_scores]).reshape(-1,1)
+
+    data['fuzzy_scores'] = fuzzy_scores
+    col_names = list(data.columns)
+    target_index = col_names.index('target')
+    fuzzy_scores_index = col_names.index('fuzzy_scores')
+    col_names[target_index], col_names[fuzzy_scores_index] = col_names[fuzzy_scores_index], col_names[target_index]
+    data = data[col_names]
+
+    return data
+
 def kfold_cv(data: np.ndarray):
     X = data[:,:-1]
     y = data[:,-1]
@@ -54,3 +69,27 @@ def kfold_cv(data: np.ndarray):
     
     return fold_sets
 
+def apply_fuzzy_score(sets: tuple, delta: float=None, k: float = None, dup: float = None):
+    train_set, test_set = sets
+
+    # adjustment of the output
+    if delta != None:
+        condition = train_set[:,-2] >= delta
+        train_set[condition,-1] = 1
+    
+    # removing instances
+    if k != None:
+        condition = train_set[:,-2] > k
+        train_set = train_set[condition]
+    
+    # duplication of the instances
+    if dup != None:
+        condition = train_set[:,-2] >= dup
+        duplicated_rows = np.copy(train_set[condition])
+        train_set = np.vstack([train_set,duplicated_rows])
+    
+    # removing fuzzy scores from training and testing sets:
+    train_set = np.concatenate((train_set[:, :train_set.shape[1]-2], train_set[:, train_set.shape[1]-1:]), axis=1)
+    test_set = np.concatenate((test_set[:, :test_set.shape[1]-2], test_set[:, test_set.shape[1]-1:]), axis=1)
+
+    return train_set, test_set
